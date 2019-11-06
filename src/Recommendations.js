8f2d9601f4d635ffaa7a5e58801e541a8fb864bd
Cleveland Stephenson <ctaylorstephenson@gmail.com>
2019-11-05 23:33:04 -0600

    add explicit construction to support state, create componentDidMount lifecycle hook to contain async http req, add conditional rendering to match

--- src/Recommendations.js
+++ src/Recommendations.js
@@ -1,36 +1,44 @@
 import React from 'react';
 
 class Recommendations extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            response: null
+        };
+    }
+
+    componentDidMount() {
         let url = 'https://api.spotify.com/v1/recommendations';
+        // FIXME: replace these hard coded api parameters
         url += '?market=US&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_tracks=0c6xIDDpzE81m2q797ordA&min_energy=0.4&min_popularity=50';
 
         let http = new XMLHttpRequest();
         http.open('GET', url);
         http.setRequestHeader('Authorization', 'Bearer BQCORkhTuDC5KmG5jJVLbg6vsgAbZMIcm-ufGbaJ_GBR6H-R5jvOxw29AcilNvc0B78z3olKqEF2OF8T_D7LRP251ff2xjPwKwehJtJwtKzSWaAa4-mIKpPlxdBTuGj99t80wmv9gQfIgjW6UsoDk23GXkc-p-U');
 
-        /* replace with synchronous version for testing
+        let thisComponent = this;
         http.onreadystatechange = function () {
-            if (http.readyState == 4 && http.status == 200) {
-            } else {
-                document.getElementById('info').innerText = http.responseType + http.responseText;
-            }
-        }
-         */
+            thisComponent.setState({
+                response: http.responseText
+            });
+        };
 
         http.send();
+    }
 
-        let response = null;
-        setTimeout(() => response = http.responseType + http.responseText, 3000);
+    render() {
+        let recommendations = "Recommendations loading...";
+        if (this.state.response != null) {
+            // FIXME: later parse response to provide only useful info to user
+            recommendations = this.state.response;
+        }
 
-        // won't return anything yet but requests are going through
         return (
             <div className="Recommendations">
-                <p>
-                    Class recommendations test
-                </p>
                 <p id="Recommendations-Body">
-                    {response}
+                    {recommendations}
                 </p>
             </div>
         );
