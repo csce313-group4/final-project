6e1dd9e82b5f2ce735a517a14188c909fdbbaced
Michael Chacko <mikechacko10@gmail.com>
2019-11-12 16:35:57 -0600

    attempting to use YoutubePlaylist. running into error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Camera from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
-import YouTube from 'react-youtube';
+import YouTubePlaylist from 'react-youtube-playlist';
 import ClipLoader from 'react-spinners/ClipLoader';
 import ImagePreview from './ImagePreview';
 import Button from 'react-bootstrap/Button';
@@ -18,7 +18,7 @@ class App extends React.Component {
             emotions: this.props.emotions,
             age: 20,
             loadSong: false,
-            playlistId: "",
+            playlistId: "PLA6E336BE9AE31719",
         };
         this.onSelectImage = this.onSelectImage.bind(this);
         this.onTakePhotoAnimationDone = this.onTakePhotoAnimationDone.bind(this);
@@ -108,52 +108,45 @@ class App extends React.Component {
     getPlaylistFromParams(musicYear, isHappy) {
         switch (musicYear) {
             case 1950:
-                if(isHappy){
+                if (isHappy) {
                     return "PLNDhBcjuPp0-UCSML4DxFqqc5WN4aGcW1";
-                }
-                else {
+                } else {
                     return "PLjLPKGJvC8TuhpLXMhskQUaC26zSBjCsM";
                 }
             case 1960:
-                if(isHappy){
+                if (isHappy) {
                     return "PLPKXtd2AJvNkaZgrb0yK3UoQk7487TceH";
-                }
-                else {
+                } else {
                     return "PLAZH8pP5KBzXiSfcH_2GgoDeXjtTUF3sM";
                 }
             case 1970:
-                if(isHappy){
+                if (isHappy) {
                     return "PL9QL1AvNabH5u2gfi48VogStrPUY_LijY";
-                }
-                else {
+                } else {
                     return "PLjyiHjKMhSuK-rChCn2Xl4RNfUcUhx-qA";
                 }
             case 1980:
-                if(isHappy){
+                if (isHappy) {
                     return "PL857_Yj5tf8cek2iOFyZBE9XSOLOhW-0m";
-                }
-                else {
+                } else {
                     return "PLT_7RtTUm0XwsUHTCuQog7xknmMCuvr4B";
                 }
             case 1990:
-                if(isHappy){
+                if (isHappy) {
                     return "PLLWfa8Ng-FdBamt3JCTadmbdd9UtLFZ7K";
-                }
-                else {
+                } else {
                     return "PLu1jpc624xfKGhWItKFNRPu7QylkwS-OR";
                 }
             case 2000:
-                if(isHappy){
+                if (isHappy) {
                     return "PLPf_PZG3-WAGq4tgjU6m87Ksndn2Jz2OF";
-                }
-                else {
+                } else {
                     return "PLIWqGUaiQxN7kL47fzQ87h0OlTaCyr8-o";
                 }
             case 2010:
-                if(isHappy){
+                if (isHappy) {
                     return "PLeZgwVkN7bbfVLcqnz9l5RjASqUkBtpBe";
-                }
-                else {
+                } else {
                     return "PL5D7fjEEs5yflZzSZAhxfgQmN6C_6UJ1W";
                 }
             default:
@@ -164,16 +157,19 @@ class App extends React.Component {
     getRecs() {
         var age = this.state.age;
         var emotions = this.state.emotions;
-        var playlistId = this.state.playlistId;
 
         //get the playlistId
         var musicYear = this.getMusicYearFromAge(age);
         var isHappy = this.isThisEmotionHappy(emotions);
-        playlistId = this.getPlaylistFromParams(musicYear, isHappy);
+        var playlistId = this.getPlaylistFromParams(musicYear, isHappy);
+        console.log("Created Playlist: " + playlistId);
 
+        console.log("Created Playlist: " + this.state.playlistId);
         this.setState({loading: true});
         // set recs
-        this.setState({playlistId: playlistId, loading: false});
+        this.setState({playlistId: playlistId, loading: false}, () => {
+            console.log("update playlist" + this.state.playlistId);
+        });
     }
 
 
@@ -185,16 +181,20 @@ class App extends React.Component {
                 autoplay: 1
             }
         };
+        console.log("loadSong: " + this.state.loadSong);
         if (this.state.loading) {
             return (<div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
                 <ClipLoader sizeUnit={"px"} size={100} color={'#36d7b7'} loading={this.state.loading}/>
             </div>)
         } else if (this.state.loadSong) {
+            console.log("Attempting to loadSong with playlistId " + this.state.playlistId);
             return (
                 <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                    <YouTube
-                        videoId="dQw4w9WgXcQ"
-                        opts={opts}
+                    <YouTubePlaylist
+                        width={640}
+                        api_key='ENTER YOUTUBE API KEY'
+                        playlist_id = {this.state.playlistId}
+                        show_thumbnails = {true}
                     />
                 </div>)
         } else {
